cb66189379554e33b92f80a7db4121ebb10d7aad
jianhuashao <jianhua.shao1986@gmail.com>
2012-05-24 21:47:48 +0100

    add dataware.slibs hello in

--- django/dataware/catalog/urls.py
+++ django/dataware/catalog/urls.py
@@ -2,4 +2,10 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^hello', 'catalog.views.hello'),
+    url(r'^slibs', 'catalog.views.hello_slibs'),
+
+    # registeration with resource
+    # INIT
+    url(r'^init_resource', 'catalog.views.init_resource'),
+
 )
--- django/dataware/catalog/views.py
+++ django/dataware/catalog/views.py
@@ -1,5 +1,14 @@
 # Create your views here.
 from django.http import HttpResponse
+import slibs_hello
 
 def hello(request):
     return HttpResponse("Hello, catalog")
+
+def hello_slibs(request):
+    slibs_hello.hello()
+    return HttpResponse('hello, dataware shared libs')
+
+#### registeration with resource ####
+def init_resource(request):
+    return HttpResponse('hello, init resource')
